87876a023465ad4c3fef23ee4972021601228254
Goodwin <dev.omollo@gmail.com>
2024-03-11 12:20:57 +0300

    Initial Commit

---
+++ quiz.py
@@ -0,0 +1,41 @@
+questions = [
+    {
+        "prompt": "What is the capital of France?",
+        "options": ["A. Paris", "B. London", "C. Berlin", "D. Madrid"],
+        "answer": "A"
+    },
+    {
+        "prompt": "Which language is primarily spoken in Brazil?",
+        "options": ["A. Spanish", "B. Portuguese", "C. English", "D. French"],
+        "answer": "B"
+    },
+    {
+        "prompt": "What is the smallest prime number?",
+        "options": ["A. 1", "B. 2", "C. 3", "D. 5"],
+        "answer": "B"
+    },
+    {
+        "prompt": "Who wrote 'To Kill a Mockingbird'?",
+        "options": ["A. Harper Lee", "B. Mark Twain", "C. J.K. Rowling", "D. Ernest Hemingway"],
+        "answer": "A"
+    }
+]
+
+def fun_quiz(questions):
+    score = 0
+    for question in questions:
+        print(question["prompt"])
+        for option in question["options"]:
+            print(option)
+
+        answer = input("Enter your answer: A,B,C or D: ").upper()
+        if answer == question["answer"]:
+            print("Correct!\n")
+            score += 1
+        else:
+            print("Wrong! Loser!\n")
+
+    print(f"You have gotten {score} out of {len(questions)} questions.")
+
+
+fun_quiz(questions)
